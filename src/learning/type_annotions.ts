56633887ba772a36bc0787927cc3fd047e28fbee
Rania <rania.ranijapple@gmail.com>
2025-08-07 09:51:21 +0200

    save

---
+++ src/learning/type_annotions.ts
@@ -0,0 +1,10 @@
+let firstName: string = "Rania";
+console.log(firstName);
+
+firstName = "Petr";
+console.log(firstName);
+
+// ? Nelze přiřadit number do string proměnné
+// firstName = 5; 
+// console.log(firstName);
+
